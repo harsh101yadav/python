10bfc40f7b0fc6e70b386250bad1fe4f0e48eef2
harsh101yadav <137609987+harsh101yadav@users.noreply.github.com>
2024-03-06 00:18:45 +0530

    Add files via upload

--- 039RaisingCustomError.py
+++ 039RaisingCustomError.py
@@ -1,7 +1,18 @@
-a = int(input("Enter number between 5 and 8 :"))
+# a = int(input("Enter number between 5 and 8 :"))
 
-if (a<5 or a>8 ):
-    raise ValueError("Number is not between 5 and 8")
-else:
-    print("Done!!")
+# if (a<5 or a>8 ):
+#     raise ValueError("Number is not between 5 and 8")
+# else:
+#     print("Done!!")
 
+try:
+    a = int(input("Enter number between "))
+    if (a<5 or a>8 ):
+        raise ValueError("Number is not between 5 and 8")
+    else:
+        print("Done!!")
+except ValueError:
+    if (a == "quit"):
+        print("Okay let's quit")
+    else:
+        raise ValueError("Enter a Number")
