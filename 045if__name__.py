3a341b776a6c6c9c596959f44d0522d9515a2d36
harsh101yadav <137609987+harsh101yadav@users.noreply.github.com>
2024-03-08 22:38:58 +0530

    Add files via upload

---
+++ 045if__name__.py
@@ -0,0 +1,4 @@
+import harsh
+
+harsh.welcome()
+# if (__name__ =="__main__" is used to run commands within a function, will a function is called in some other file then it won't run it commands within it)
