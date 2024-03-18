3b0d2b98fe1ec49d12ebb2a30090297fe994117f
harsh101yadav <137609987+harsh101yadav@users.noreply.github.com>
2024-03-19 00:27:10 +0530

    Add files via upload

---
+++ 078SingleInheritance.py
@@ -0,0 +1,34 @@
+class Animal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        
+    def make_sound(self):
+        print("Sound made by the animal")
+
+class Dog(Animal):
+    def __init__(self, name, breed):
+        Animal.__init__(self, name, species="Dog")
+        self.breed = breed
+        
+    def make_sound(self):
+        print("Bark!")
+
+class Cat(Animal):
+    def __init__(self, name, species):
+        Animal.__init__(self,name,species="Cat")
+
+    def make_sound(self):
+        print("Meow")
+
+d = Dog("Dog", "Doggerman")
+d.make_sound()
+
+a = Animal("Dog", "Dog")
+a.make_sound()
+
+# Quick Quiz: Implement a Cat class by using the animal class. 
+#Add some methods specific to cat
+
+a = Cat("Cat","Sphynx")
+a.make_sound()
