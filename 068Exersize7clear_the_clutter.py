09369accb046e4d78a010583913f8f8cad4fd234
harsh101yadav <137609987+harsh101yadav@users.noreply.github.com>
2024-03-18 22:08:37 +0530

    Add files via upload

--- 068Exersize7clear_the_clutter.py
+++ 068Exersize7clear_the_clutter.py
@@ -7,9 +7,31 @@ Do the same for other file formats. For example:
 sfdsf.png --> 1.png
 vfsf.png --> 2.png
 this.png --> 3.png
+
 design.png --> 4.png
 name.png --> 5.png
 """
 
 import os
+# print(os.__dict__)
+# print(help(os))
 
+def Clear_folder(folder_path):
+    files = os.listdir(folder_path)
+    print(files)
+    new_files =[]
+    for file in files:
+        if file.endswith(".png"):
+            new_files.append(file)
+
+    print(new_files)
+    for i,file in enumerate(new_files):
+        new_files_names = f"{i+1}.png"
+        os.chdir("D:\Study Material\Subjects\Python\Clutter")
+        os.rename(file, new_files_names)
+    
+
+
+folder_path = "D:\Study Material\Subjects\Python\Clutter"
+
+Clear_folder(folder_path)
